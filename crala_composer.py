22e215dbf47067a5d4111c0b070def7b0fbb3531
luojie <luoxiaojie958@gmail.com>
2017-01-03 09:45:02 +0800

    project0

--- crala_composer.py
+++ crala_composer.py
@@ -5,24 +5,58 @@ nodes = []
 packages = []
 subs = []
 pubs = []
+_ids = []
 
 client = MongoClient()
 db = client.talker
 
-cursor = db.talker.find({"node":"talker"})
+cursor = db.talker.find({"node":"listener"})
 for document in cursor:
 	nodes.append(document["node"])
 	packages.append(document["package"])
 	subs.append(document["sub"])
 	pubs.append(document["pub"])
-i=0
+	_ids.append(document["_id"])
+
 for i in range(len(nodes)):
 	cursor = db.talker.find({"sub":pubs[i]})
 	for document in cursor:
-		nodes.append(document["node"])
-		packages.append(document["package"])
-		subs.append(document["sub"])
-		pubs.append(document["pub"])
+		if document["_id"] not in _ids:
+			nodes.append(document["node"])
+			packages.append(document["package"])
+			subs.append(document["sub"])
+			pubs.append(document["pub"])
+			_ids.append(document["_id"])
+
+	cursor = db.talker.find({"pub":subs[i]})
+	for document in cursor:
+		if document["_id"] not in _ids:
+			nodes.append(document["node"])
+			packages.append(document["package"])
+			subs.append(document["sub"])
+			pubs.append(document["pub"])
+			_ids.append(document["_id"])
+for i in range(len(nodes)):
+	cursor = db.talker.find({"sub":pubs[i]})
+	for document in cursor:
+		if document["_id"] not in _ids:
+			nodes.append(document["node"])
+			packages.append(document["package"])
+			subs.append(document["sub"])
+			pubs.append(document["pub"])
+			_ids.append(document["_id"])
+
+	cursor = db.talker.find({"pub":subs[i]})
+	for document in cursor:
+		if document["_id"] not in _ids:
+			nodes.append(document["node"])
+			packages.append(document["package"])
+			subs.append(document["sub"])
+			pubs.append(document["pub"])
+			_ids.append(document["_id"])
+
+print(str(len(nodes))+" nodes")
+
 for j in nodes:
 	print(j)
 
