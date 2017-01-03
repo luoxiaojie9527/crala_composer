319c0cbe521c7f79b6ef4c7cf8abd122797ca069
luojie <luoxiaojie958@gmail.com>
2017-01-03 10:45:25 +0800

    project0

--- crala_composer.py
+++ crala_composer.py
@@ -6,6 +6,8 @@ packages = []
 subs = []
 pubs = []
 _ids = []
+num = []
+flag=1
 
 client = MongoClient()
 db = client.talker
@@ -17,43 +19,34 @@ for document in cursor:
 	subs.append(document["sub"])
 	pubs.append(document["pub"])
 	_ids.append(document["_id"])
+num.append(len(nodes))
 
-for i in range(len(nodes)):
-	cursor = db.talker.find({"sub":pubs[i]})
-	for document in cursor:
-		if document["_id"] not in _ids:
-			nodes.append(document["node"])
-			packages.append(document["package"])
-			subs.append(document["sub"])
-			pubs.append(document["pub"])
-			_ids.append(document["_id"])
+while(flag):
+	for i in range(len(nodes)):
+		cursor = db.talker.find({"sub":pubs[i]})
+		for document in cursor:
+			if document["_id"] not in _ids:
+				nodes.append(document["node"])
+				packages.append(document["package"])
+				subs.append(document["sub"])
+				pubs.append(document["pub"])
+				_ids.append(document["_id"])
 
-	cursor = db.talker.find({"pub":subs[i]})
-	for document in cursor:
-		if document["_id"] not in _ids:
-			nodes.append(document["node"])
-			packages.append(document["package"])
-			subs.append(document["sub"])
-			pubs.append(document["pub"])
-			_ids.append(document["_id"])
-for i in range(len(nodes)):
-	cursor = db.talker.find({"sub":pubs[i]})
-	for document in cursor:
-		if document["_id"] not in _ids:
-			nodes.append(document["node"])
-			packages.append(document["package"])
-			subs.append(document["sub"])
-			pubs.append(document["pub"])
-			_ids.append(document["_id"])
-
-	cursor = db.talker.find({"pub":subs[i]})
-	for document in cursor:
-		if document["_id"] not in _ids:
-			nodes.append(document["node"])
-			packages.append(document["package"])
-			subs.append(document["sub"])
-			pubs.append(document["pub"])
-			_ids.append(document["_id"])
+		cursor = db.talker.find({"pub":subs[i]})
+		for document in cursor:
+			if document["_id"] not in _ids:
+				nodes.append(document["node"])
+				packages.append(document["package"])
+				subs.append(document["sub"])
+				pubs.append(document["pub"])
+				_ids.append(document["_id"])
+	num.append(len(nodes))
+	print(num[-1])
+	if num[-1]!=num[len(num)-2]:
+		flag=1
+	else:
+		flag=0
+	
 
 print(str(len(nodes))+" nodes")
 
