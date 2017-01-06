b996821061197b0b58db4208484d6b79b4869df8
luojie <luoxiaojie958@gmail.com>
2017-01-06 21:39:27 +0800

    project0

--- crala_composer.py
+++ crala_composer.py
@@ -5,82 +5,97 @@ import threading
 import subprocess
 import os
 
-nodes = []
-packages = []
-subs = []
-pubs = []
-_ids = []
-num = []
-flag=1
 
-
-client = MongoClient()
-db = client.tasks
-cursor = db.tasks.find({"task":"drawsquare_turtlesim"})
-for document in cursor:
-	initial = document["node"]
-
-db = client.talker
-
-cursor = db.talker.find({"node":initial})
-for document in cursor:
-	nodes.append(document["node"])
-	packages.append(document["package"])
-	subs.append(document["sub"])
-	pubs.append(document["pub"])
-	_ids.append(document["_id"])
-num.append(len(nodes))
-
-while(flag):
-	for i in range(len(nodes)):
-		if pubs[i] <> "":
-			print(pubs[i])
-			cursor = db.talker.find({"sub":pubs[i]})
-			for document in cursor:
-				if document["_id"] not in _ids:
-					nodes.append(document["node"])
-					packages.append(document["package"])
-					subs.append(document["sub"])
-					pubs.append(document["pub"])
-					_ids.append(document["_id"])
-		if subs[i] <> "":
-			cursor = db.talker.find({"pub":subs[i]})
-			for document in cursor:
-				if document["_id"] not in _ids:
-					nodes.append(document["node"])
-					packages.append(document["package"])
-					subs.append(document["sub"])
-					pubs.append(document["pub"])
-					_ids.append(document["_id"])
-
+#######################################################################################################################################
+def find_nodes(nodes):
+	flag = True
+	while(flag):
+		for i in range(len(nodes)):
+			if pubs[i] <> "":
+				cursor = db.nodes_collection.find({"sub":pubs[i]})
+				for document in cursor:
+					if document["_id"] not in _ids:
+						nodes.append(document["node"])
+						packages.append(document["package"])
+						subs.append(document["sub"])
+						pubs.append(document["pub"])
+						_ids.append(document["_id"])
+		
+			if subs[i] <> "":
+				cursor = db.nodes_collection.find({"pub":subs[i]})
+				for document in cursor:
+					if document["_id"] not in _ids:
+						nodes.append(document["node"])
+						packages.append(document["package"])
+						subs.append(document["sub"])
+						pubs.append(document["pub"])
+						_ids.append(document["_id"])
+		num.append(len(nodes))
+		if num[-1]!=num[len(num)-2]:
+			flag=True
+		else:
+			flag=False
+#####################################################################################################################################
+def get_task(task):
+	cursor = db.tasks.find({"task":task})
+	for document in cursor:
+		initial=document["node"]
+	return initial
+#####################################################################################################################################
+def find_initial_node(initial):
+	for i in range(len(initial)):
+		cursor = db.nodes_collection.find({"node":initial[i]})
+		for document in cursor:
+			nodes.append(document["node"])
+			packages.append(document["package"])
+			subs.append(document["sub"])
+			pubs.append(document["pub"])
+			_ids.append(document["_id"])
 	num.append(len(nodes))
-	print(num[-1])
-	if num[-1]!=num[len(num)-2]:
-		flag=1
-	else:
-		flag=0
+#####################################################################################################################################
+
+
 	
-
-print(str(len(nodes))+" nodes")
-
-for j in nodes:
-	print(j)
-
-
-threads = []
-threads.append(threading.Thread(target=subprocess.Popen(('roscore &'),shell=True)))
-for i in range(len(nodes)):
-	#threads.append(threading.Thread(target=os.system("ls")))
-	threads.append(threading.Thread(target=subprocess.Popen(("rosrun"+" "+packages[i]+" "+nodes[i]+" &"),shell=True)))
-os.system("rosrun rqt_graph rqt_graph")
-if __name__ == '__main__':
+#####################################################################################################################################
+def threads_run():
+	threads = []
+	threads.append(threading.Thread(target=subprocess.Popen(('roscore '),shell=True)))
+	for i in range(len(nodes)):
+		threads.append(threading.Thread(target=subprocess.Popen(("rosrun"+" "+packages[i]+" "+nodes[i]+" "),shell=True)))
+	
 	for t in threads:
-	    t.setDaemon(True)
-	    t.start()
+		t.setDaemon(True)
+		t.start()
+	os.system("rosrun rqt_graph rqt_graph")
 	t.join()
+#####################################################################################################################################	
+	
+if __name__ == '__main__':
+	nodes = [] 
+	packages = [] 
+	subs = [] 
+	pubs = [] 
+	_ids = [] 
+	num = [] 
+
+	client = MongoClient()
+	db = client.tasks
+	initial=get_task("drawsquare_turtlesim")
+	
+	client = MongoClient()
+	db = client.nodes_db
+	find_initial_node(initial)
+	find_nodes(nodes)
+
+	print(str(len(nodes))+" nodes")
+
+	for j in nodes:
+		print(j)
+	
+	threads_run()
 	
 	print "all over"
-
+################################################GAMEOVER####################################################################
 
 
 
