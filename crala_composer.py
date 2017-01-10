8281031700738ab0d62040ec3dd6174e34966625
luojie <luoxiaojie958@gmail.com>
2017-01-10 20:46:24 +0800

    project0

--- crala_composer.py
+++ crala_composer.py
@@ -6,26 +6,29 @@ import subprocess
 import os
 
 
-
 #######################################################################################################################################
 def find_nodes(nodes):
 	flag = True
 	while(flag):
 		for i in range(len(nodes)):
-			if pubs[i] <> "":
-				cursor = db.nodes_collection.find({"sub":pubs[i]})
-				for document in cursor:
-					if document["_id"] not in _ids:
+			if pubs[0] and pubs[i]:
+				cursor = db.nodes_collection.find_one({"sub":pubs[i]})
+#				for document in cursor:
+				if cursor:
+					document = cursor
+					if document["_id"] not in _ids and document["sub"] not in subs and document["pub"] not in pubs:
 						nodes.append(document["node"])
 						packages.append(document["package"])
 						subs.append(document["sub"])
 						pubs.append(document["pub"])
 						_ids.append(document["_id"])
 		
-			if subs[i] <> "":
-				cursor = db.nodes_collection.find({"pub":subs[i]})
-				for document in cursor:
-					if document["_id"] not in _ids:
+			if subs[0] and subs[i]:
+				cursor = db.nodes_collection.find_one({"pub":subs[i]})
+#				for document in cursor:
+				if cursor:
+					document = cursor
+					if document["_id"] not in _ids and document["sub"] not in subs and document["pub"] not in pubs:
 						nodes.append(document["node"])
 						packages.append(document["package"])
 						subs.append(document["sub"])
@@ -36,23 +39,29 @@ def find_nodes(nodes):
 			flag=True
 		else:
 			flag=False
+
 #####################################################################################################################################
 def get_task(task):
-	cursor = db.tasks.find({"task":task})
-	for document in cursor:
-		initial=document["node"]
+	cursor = db.tasks.find_one({"task":task})
+#	for document in cursor:
+	document = cursor
+	initial=document["node"]
 	return initial
 #####################################################################################################################################
 def find_initial_node(initial):
 	for i in range(len(initial)):
-		cursor = db.nodes_collection.find({"node":initial[i]})
-		for document in cursor:
+		cursor = db.nodes_collection.find_one({"node":initial[i]})
+#		for document in cursor:
+		if cursor:
+			document = cursor
 			nodes.append(document["node"])
 			packages.append(document["package"])
 			subs.append(document["sub"])
 			pubs.append(document["pub"])
 			_ids.append(document["_id"])
 	num.append(len(nodes))
+	print(num[-1])
+
 #####################################################################################################################################
 
 
@@ -70,31 +79,36 @@ def threads_run():
 	os.system("rosrun rqt_graph rqt_graph")
 	t.join()
 #####################################################################################################################################	
-	
+
 if __name__ == '__main__':
-	nodes = [] 
-	packages = [] 
-	subs = [] 
-	pubs = [] 
-	_ids = [] 
-	num = [] 
+	nodes = []
+	packages = []
+	subs = []
+	pubs = []
+	_ids = []
+	num = []
 
 	client = MongoClient()
 	db = client.tasks
-	initial=get_task("drawsquare_turtlesim")
-	
+	initial = get_task("talk and listen")
+
+	print(initial)
+
 	client = MongoClient()
 	db = client.nodes_db
 	find_initial_node(initial)
-	find_nodes(nodes)
+	print(nodes)
 
-	print(str(len(nodes))+" nodes")
+	find_nodes(nodes)
+	print(nodes)
+
+	print(str(len(nodes)) + " nodes")
 
 	for j in nodes:
 		print(j)
-	
+
 	threads_run()
-	
+
 	print "all over"
 ################################################GAME OVER############################################################################
 
